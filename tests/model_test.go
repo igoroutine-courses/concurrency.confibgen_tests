184113e962976b4826ea1b85f319037479c6b43e
Igor Walther <igorfeedigorfeed@gmail.com>
2025-11-30 20:55:16 +0300

    feat: add tests

---
+++ tests/model_test.go
@@ -0,0 +1,118 @@
+//go:build model_test
+
+package fibonacci
+
+import (
+	"slices"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// TODO: в ридми начало с 1
+
+func TestGoldenSequence(t *testing.T) {
+	t.Parallel()
+
+	generator := NewGenerator()
+	fibSequence := make([]uint64, 0, 9)
+
+	expected := [9]uint64{0, 1, 1, 2, 3, 5, 8, 13, 21}
+
+	for range 9 {
+		fibSequence = append(fibSequence, generator.Next())
+	}
+
+	require.Equal(t, expected[:], fibSequence)
+}
+
+func TestOverflow(t *testing.T) {
+	t.Parallel()
+	generator := NewGenerator()
+
+	var prev uint64
+	for range maxFibonacciNumber {
+		cur := generator.Next()
+		require.LessOrEqual(t, prev, cur)
+
+		prev = cur
+	}
+}
+
+func TestGolden(t *testing.T) {
+	t.Parallel()
+
+	// F_92 = 7540113804746346429
+	const expected uint64 = 7540113804746346429
+
+	generator := NewGenerator()
+	var cur uint64
+
+	for range maxFibonacciNumber + 1 {
+		cur = generator.Next()
+	}
+
+	require.EqualValues(t, expected, cur)
+}
+
+func TestPanicOnOverflow(t *testing.T) {
+	t.Parallel()
+	generator := NewGenerator()
+
+	var prev uint64
+	for range maxFibonacciNumber + 1 {
+		cur := generator.Next()
+		require.LessOrEqual(t, prev, cur)
+
+		prev = cur
+	}
+
+	func() {
+		defer func() {
+			err := recover()
+			require.NotNil(t, err, "expected panic on overflow")
+
+			vErr, ok := err.(error)
+			require.True(t, ok, "expected panic with error on overflow")
+
+			require.ErrorContains(t, vErr, "overflow", "expected verbose message on overflow")
+		}()
+
+		generator.Next()
+	}()
+}
+
+func TestFibonacciInvariant(t *testing.T) {
+	t.Parallel()
+
+	workers := 10
+	generator := NewGenerator()
+	const iters = 9
+
+	wg := new(sync.WaitGroup)
+	workerValues := make([][]uint64, workers)
+	for workerID := range workers {
+		workerValues[workerID] = make([]uint64, 0, iters)
+	}
+
+	for workerID := range workers {
+		wg.Go(func() {
+			for range iters {
+				value := generator.Next()
+				workerValues[workerID] = append(workerValues[workerID], value)
+			}
+		})
+	}
+
+	wg.Wait()
+
+	allValues := make([]uint64, 0, iters*workers)
+	for _, values := range workerValues {
+		require.Truef(t, slices.IsSorted(values), "expected increasing sequence of values, got: %v", values)
+		allValues = append(allValues, values...)
+	}
+
+	slices.Sort(allValues)
+	require.True(t, isFibonacci(t, allValues), "expected fibonacci sequence")
+}
---
+++ tests/performance_test.go
@@ -0,0 +1,80 @@
+//go:build performance_test
+
+package fibonacci
+
+import (
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPerformance(t *testing.T) {
+	solution := testing.Benchmark(func(b *testing.B) {
+		b.ReportAllocs()
+
+		generator := NewGenerator()
+		counter := 0
+
+		for b.Loop() {
+			if counter == maxFibonacciNumber {
+				b.StopTimer()
+				generator = NewGenerator()
+				counter = 0
+				b.StartTimer()
+			}
+
+			generator.Next()
+			counter++
+			time.Sleep(time.Millisecond)
+		}
+	})
+
+	emulation := testing.Benchmark(func(b *testing.B) {
+		b.ReportAllocs()
+
+		counter := 0
+
+		var prev, cur uint64 = 0, 1
+		for b.Loop() {
+			if counter == maxFibonacciNumber {
+				b.StopTimer()
+				counter = 0
+				prev, cur = 0, 1
+				b.StartTimer()
+			}
+
+			tmp := prev
+			atomic.CompareAndSwapUint64(&prev, prev, cur)
+			cur += tmp
+
+			counter++
+			time.Sleep(time.Millisecond)
+		}
+	})
+
+	actual := float64(solution.NsPerOp()) / float64(emulation.NsPerOp())
+	require.LessOrEqual(t, actual, 1.05)
+}
+
+func TestMallocs(t *testing.T) {
+	generator := NewGenerator()
+	mallocs := inspectMallocs(t, func() {
+		generator.Next()
+	})
+
+	require.Zero(t, mallocs, "expected zero allocations on Next call")
+}
+
+func TestNumGoroutines(t *testing.T) {
+	generator := NewGenerator()
+
+	gNum := inspectNumGoroutines(t, func() {
+		for range maxFibonacciNumber {
+			generator.Next()
+		}
+	})
+
+	require.Equal(t, 2, gNum)
+}
